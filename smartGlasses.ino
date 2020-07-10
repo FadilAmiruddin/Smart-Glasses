703baf6ad72be5c1d92f785bb3ec15ba92ed052a
Zuraxle <64388495+Zuraxle@users.noreply.github.com>
2020-07-10 17:05:17 -0400

    Add files via upload
    The Final paper describes most the things in the project and how it works picture breaks down this complex process to a flowchart. 100X1 is the poster for this project and Smart Glasses.ino is the arduino file. The speech to text apk is the android application and the Speech to Text.aia is the code

---
+++ smartGlasses.ino
@@ -0,0 +1,69 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define NUMFLAKES     10 // Number of snowflakes in the animation example
+
+#define LOGO_HEIGHT   16
+#define LOGO_WIDTH    16
+char incoming;
+boolean newData;
+String t;
+  int x = 1;
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  ESP_BT.begin("sst2")
+  Serial.println("Starting bluetooth")
+  rtc.begin()
+
+}
+
+void loop() {
+  display.clearDisplay();
+
+  display.setTextSize(2); // Draw 2X-scale text
+  display.setTextColor(SSD1306_WHITE);
+if(ESP32.avalaible()){
+    while(x==0){
+
+      t=("")
+      x= x+1
+    }
+    else{
+      display.println((t))
+      x=0
+    }
+    }
+}
+}
